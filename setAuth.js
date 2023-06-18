d9932037d67d87a133b54fe2729c54d689e1422e
n3n7i <25413904+n3n7i@users.noreply.github.com>
2023-06-18 10:10:42 +0800

    Create setAuth.js

---
+++ setAuth.js
@@ -0,0 +1,17 @@
+/*export const setAuthorityInstructionData = struct<SetAuthorityInstructionData>([
+    u8('instruction'),
+    u8('authorityType'),
+    u8('newAuthorityOption'),
+    publicKey('newAuthority'),
+]);
+
+export enum AuthorityType {
+    MintTokens = 0,
+    FreezeAccount = 1,
+    AccountOwner = 2,
+    CloseAccount = 3,
+}
+
+*/
+
+
